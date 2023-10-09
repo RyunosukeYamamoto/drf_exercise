c23bcad1384b1022468b7c0feb66957c3ee14811
RyunosukeYamamoto <ryunosukeyamamoto0@gmail.com>
2023-10-09 21:29:39 +0900

    トークン取得じゃなくて発酵させる

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 from . import views
 from rest_framework.authtoken import views as auth_views
+from .auth import CustomAuthToken
 
 # router = routers.DefaultRouter()
 # router.register(r"articles", views.ArticleViewSet)
@@ -10,5 +11,5 @@ urlpatterns = [
     # path("", include(router.urls)),
     path("articles/", views.ArticleAPIlistCreate.as_view()),
     path("articles/<int:pk>/", views.ArticleAPIRetrieveUpdateDestroy.as_view()),
-    path("create_token/", auth_views.obtain_auth_token),
+    path("create_token/", CustomAuthToken.as_view()),
 ]
